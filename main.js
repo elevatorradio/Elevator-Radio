7e57d3bf8a3e696eba0bf71429b297ffd3b85f58
Kirsten Spruit <58689882+kirstenspruit@users.noreply.github.com>
2022-09-18 15:34:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
         if (opendoor === true) {
             // $(".logo").css("display", "flex")
-            document.getElementById("rd-top").innerHTML = "● Live from Milan 08–10.06";
+            document.getElementById("rd-top").innerHTML = "●";
             //  document.getElementById("phone-top").innerHTML = 'ELEVATOR RADIO - Thu 20.05.2021<br> 16:00–20:00 CEST <img  src="DAE_logo_white.png">;'
             document.getElementById("phone").innerHTML = ' <h1 id=phone-top>ELEVATOR RADIO </h1><br><br>'
         }
